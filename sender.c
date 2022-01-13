febfbf7727f91c64da83b59bb75b8c946037de4c
trangnguyen3010 <66387098+trangnguyen3010@users.noreply.github.com>
2022-01-12 22:06:12 -0600

    Update sender.c

--- sender.c
+++ sender.c
@@ -1,5 +1,5 @@
 /*
-** talker.c -- a datagram "client" demo
+** server.c -- a stream socket server demo
 */
 
 #include <stdio.h>
@@ -10,84 +10,149 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-#include <arpa/inet.h>
 #include <netdb.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#include <signal.h>
 
-#define SERVERPORT "4950" // the port users will be connecting to
+#define SENDPORT "33490" // the port users will be connecting to
+
+#define BACKLOG 10 // how many pending connections queue will hold
+
+void sigchld_handler(int s)
+{
+    (void)s; // quiet unused variable warning
+
+    // waitpid() might overwrite errno, so we save and restore it:
+    int saved_errno = errno;
+
+    while (waitpid(-1, NULL, WNOHANG) > 0)
+        ;
+
+    errno = saved_errno;
+}
+
+// get sockaddr, IPv4 or IPv6:
+void *get_in_addr(struct sockaddr *sa)
+{
+    if (sa->sa_family == AF_INET)
+    {
+        return &(((struct sockaddr_in *)sa)->sin_addr);
+    }
+
+    return &(((struct sockaddr_in6 *)sa)->sin6_addr);
+}
+
+void print_IP(struct addrinfo *res){
+    struct addrinfo *p;
+    char ipstr[INET6_ADDRSTRLEN];
+    for(p = res;p != NULL; p = p->ai_next) {
+        void *addr;
+        char *ipver;
+
+        // get the pointer to the address itself,
+        // different fields in IPv4 and IPv6:
+        if (p->ai_family == AF_INET) { // IPv4
+            struct sockaddr_in *ipv4 = (struct sockaddr_in *)p->ai_addr;
+            addr = &(ipv4->sin_addr);
+            ipver = "IPv4";
+        } else { // IPv6
+            struct sockaddr_in6 *ipv6 = (struct sockaddr_in6 *)p->ai_addr;
+            addr = &(ipv6->sin6_addr);
+            ipver = "IPv6";
+        }
+
+        // convert the IP to a string and print it:
+        inet_ntop(p->ai_family, addr, ipstr, sizeof ipstr);
+        printf("  %s: %s\n", ipver, ipstr);
+    }
+}
 
 int main(int argc, char *argv[])
 {
-	int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
+    if (argc != 3)
+    {
+        fprintf(stderr, "usage: talker hostname port\n");
+        exit(1);
+    }
 
-	if (argc != 2)
-	{
-		fprintf(stderr, "usage: talker hostname\n");
-		exit(1);
-	}
+    int sockfd, new_fd; // listen on sock_fd, new connection on new_fd
+    struct addrinfo hints, *servinfo, *p;
+    struct sockaddr_storage their_addr; // connector's address information
+    socklen_t sin_size;
+    struct sigaction sa;
+    int yes = 1;
+    char s[INET6_ADDRSTRLEN];
+    int rv;
 
-	memset(&hints, 0, sizeof hints);
-	hints.ai_family = AF_INET6; // set to AF_INET to use IPv4
-	hints.ai_socktype = SOCK_DGRAM;
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_UNSPEC;
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
 
-	if ((rv = getaddrinfo(argv[1], SERVERPORT, &hints, &servinfo)) != 0)
-	{
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
+    //listen to IP:argv[1] at port SENDPORT
+    if ((rv = getaddrinfo(argv[1], argv[2], &hints, &servinfo)) != 0)
+    {
+        fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+        return 1;
+    }
 
-	// loop through all the results and make a socket
-	for (p = servinfo; p != NULL; p = p->ai_next)
-	{
-		if ((sockfd = socket(p->ai_family, p->ai_socktype, p->ai_protocol)) == -1)
-		{
-			perror("talker: socket");
-			continue;
-		}
+    // loop through all the results and bind to the first we can
+    for (p = servinfo; p != NULL; p = p->ai_next)
+    {
+        if ((sockfd = socket(p->ai_family, p->ai_socktype,
+                             p->ai_protocol)) == -1)
+        {
+            perror("client: socket");
+            continue;
+        }
 
-		break;
-	}
+        if (connect(sockfd, p->ai_addr, p->ai_addrlen) == -1)
+        {
+            perror("client: connect");
+            close(sockfd);
+            continue;
+        }
 
-	if (p == NULL)
-	{
-		fprintf(stderr, "talker: failed to create socket\n");
-		return 2;
-	}
+        break;
+    }
 
-	for(;;)
-	{
-		printf("Please enter a line:\n");
-		char *line = NULL;
-		size_t len = 0;
-		ssize_t lineSize = 0;
-		lineSize = getline(&line, &len, stdin); //getline does malloc automatically on line
-		line[lineSize - 1] = 0; // null terminated
-		printf("You entered '%s', which has %zu chars.\n", line, lineSize - 1);
+    if (p == NULL)
+    {
+        fprintf(stderr, "client: failed to connect\n");
+        return 2;
+    }
 
-		// concatenate the strings to be in the form "hostname, port: message"
-		char *str = malloc(sizeof(char) * (strlen(argv[1]) + strlen(SERVERPORT) + lineSize + 5));
-		strncat(str, argv[1], strlen(argv[1]));
-		strncat(str, ", ", 2);
-		strncat(str, SERVERPORT, strlen(SERVERPORT));
-		strncat(str, ": ", 2);
-		strncat(str, line, lineSize);
-		str[strlen(str)] = 0; // null-terminated
+    inet_ntop(p->ai_family, get_in_addr((struct sockaddr *)p->ai_addr),
+              s, sizeof s);
+    printf("client: connecting to %s\n", s);
 
-		if ((numbytes = sendto(sockfd, str, strlen(str), 0, p->ai_addr, p->ai_addrlen)) == -1)
-		{
-			perror("talker: sendto");
-			exit(1);
-		}
-		printf("talker: sent %d bytes to %s\n", numbytes, str);
+    freeaddrinfo(servinfo); // all done with this structure
 
-		free(line);
-		free(str);
-	}
+    int numbytes;
 
-	freeaddrinfo(servinfo);
-	close(sockfd);
+    while (1)
+    { // main accept() loop
+        printf("Please enter a line:\n");
+        char *line = NULL;
+        size_t len = 0;
+        ssize_t lineSize = 0;
+        lineSize = getline(&line, &len, stdin); //getline does malloc automatically on line
+        line[lineSize - 1] = 0;                 // null terminated
+        printf("You entered '%s', which has %zu chars.\n", line, lineSize - 1);
 
-	return 0;
+        // send a message
+        if (!fork())
+        {                  // this is the child process
+            if (send(sockfd, line, lineSize, 0) == -1)
+                perror("send");
+            exit(0);
+        }
+
+        free(line);
+        line = 0;
+    }
+    close(sockfd);
+
+    return 0;
 }
